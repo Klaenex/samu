695904e5f2b03bf2a66e9b157290155f4e84be77
Klaenex <klaenex.29@gmail.com>
2022-04-14 19:45:53 +0200

    mdp reset

--- functions/modifyPassword.php
+++ functions/modifyPassword.php
@@ -1,5 +1,14 @@
 <?php
 require './emailFunctions.php';
-
-echo 'test';
-// 
+require './db.php';
+require './alert.php';
+$email = filter_var($_POST['modify_password_email'], FILTER_SANITIZE_EMAIL);
+$sql = "SELECT id FROM users WHERE email='$email'";
+$req = mysqli_query($db, $sql);
+$user = mysqli_fetch_assoc($req);
+if ($user) {
+    emailNewPassword($email, $user['id'], $db);
+    success('Un email vous a été envoyé pour modifier votre mot de passe');
+} else {
+    error('Cet email n\'existe pas');
+}
--- templates/pages/modifyPassword.php
+++ templates/pages/modifyPassword.php
@@ -2,11 +2,11 @@
     <div class="wrapper wrapper_arrow" data-page="modify_password">
         <img src="./images/arrow-left.svg" alt="back last menu" class="img img-arrow">
     </div>
-    <h2 class="title">Modifier un évènement</h2>
+    <h2 class="title">Récupération de mot de passe</h2>
     <form class="form" id="modify_password" method="post" data-form="modify_password">
         <div class="wrapper wrapper_input">
-            <label for="modify_password-email">Email</label>
-            <input type="email" name="modify_password-email" id="modify_password-email" placeholder="Email">
+            <label for="modify_password_email">Email</label>
+            <input type="email" name="modify_password_email" id="modify_password_email" placeholder="Email">
         </div>
         <input type="submit" value="Envoyer">
     </form>
